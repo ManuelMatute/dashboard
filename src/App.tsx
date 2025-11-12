5c1b16d87acc39e6cc95176ac547b256a595a449
ManuelMatute <149851692+ManuelMatute@users.noreply.github.com>
2025-11-12 19:05:23 +0000

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import { Grid } from '@mui/material';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Grid from '@mui/material/Grid'  // <- Grid v2
 import './App.css'
 
 function App() {
@@ -10,11 +8,20 @@ function App() {
   return (
     <>
       <div>
-            <h1>Bienvenido al Dashboard</h1>
-        </div>
-      
-      
+        <h1>Bienvenido al Dashboard</h1>
+      </div>
 
+      <Grid container spacing={5} justifyContent="center" alignItems="center">
+        {/* Gráfico (oculto en xs, visible desde md) */}
+        <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: 'none', md: 'block' } }}>
+          Elemento: Gráfico
+        </Grid>
+
+        {/* Tabla (oculta en xs, visible desde md) */}
+        <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: 'none', md: 'block' } }}>
+          Elemento: Tabla
+        </Grid>
+      </Grid>
     </>
   )
 }
